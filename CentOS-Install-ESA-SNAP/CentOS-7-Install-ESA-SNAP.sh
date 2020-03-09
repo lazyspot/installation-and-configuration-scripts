bc20e35640ccfd1a0dd833e93c4eea2d3f98d402
Marcin <31159047+lazyspot@users.noreply.github.com>
2020-03-09 14:11:10 +0100

    Modify installing ESA SNAP

--- CentOS-Install-ESA-SNAP/CentOS-7-Install-ESA-SNAP.sh
+++ CentOS-Install-ESA-SNAP/CentOS-7-Install-ESA-SNAP.sh
@@ -3,6 +3,7 @@ yum install -y java
 yum install -y wget
 wget http://step.esa.int/downloads/7.0/installers/esa-snap_all_unix_7_0.sh -O /tmp/esa-snap_all_unix_7_0.sh
 sudo chmod 755 /tmp/esa-snap_all_unix_7_0.sh
-bash /tmp/esa-snap_all_unix_7_0.sh -q
+sudo mkdir -p /opt/snap/
+sudo bash /tmp/esa-snap_all_unix_7_0.sh -q -dir /opt/snap/
 rm -rf /tmp/esa-snap_all_unix_7_0.sh
 #gpt path: gptPath="/opt/snap/bin/gpt"
