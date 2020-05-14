6ffd7ec92ab9e7591c4f5800a44e98cf9e23704a
Marcin <31159047+lazyspot@users.noreply.github.com>
2020-05-14 13:58:53 +0200

    Add chgange permission to shared folder Ubuntu

--- Linux/Ubuntu/VirtualBox-guest-additions/install.sh
+++ Linux/Ubuntu/VirtualBox-guest-additions/install.sh
@@ -2,3 +2,11 @@ sudo apt-get update -y
 sudo apt-get install -y make
 sudo apt-get install -y gcc
 sudo apt-get install -y perl
+#Add permission to shared folder
+if [ "$1" == "" ]; then
+	USER="$USER"
+	else
+	USER=$1
+fi
+sudo usermod -aG vboxsf ${USER}
+echo "Should reboot"
