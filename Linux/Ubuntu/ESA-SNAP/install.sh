55d75ee9c329980a7756d6093f205e276fcbbe07
Marcin <31159047+lazyspot@users.noreply.github.com>
2021-06-04 11:13:24 +0200

    Update install.sh

--- Linux/Ubuntu/ESA-SNAP/install.sh
+++ Linux/Ubuntu/ESA-SNAP/install.sh
@@ -6,3 +6,6 @@ sudo mkdir -p /opt/snap/
 sudo bash /tmp/esa-snap_all_unix_${ESA_SNAP_VERSION:0:1}_${ESA_SNAP_VERSION:2:3}.sh -q -dir /opt/snap/
 rm -rf /tmp/esa-snap_all_unix_${ESA_SNAP_VERSION:0:1}_${ESA_SNAP_VERSION:2:3}.sh
 #gpt path: gptPath="/opt/snap/bin/gpt"
+#sudo snap --nosplash --nogui --modules --list --refresh
+sudo snap --nosplash --nogui --modules --update-all
+sudo bash download-srtm.sh
